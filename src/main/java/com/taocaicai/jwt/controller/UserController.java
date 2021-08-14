da1b5a2feb6568a3eee120474f8fc9e5bccad9af
Oakley <cqfangchaoit@sina.com>
2021-08-15 05:49:16 +0800

    update

--- src/main/java/com/taocaicai/jwt/controller/UserController.java
+++ src/main/java/com/taocaicai/jwt/controller/UserController.java
@@ -5,6 +5,7 @@ import com.taocaicai.jwt.service.UserService;
 import com.taocaicai.jwt.util.TokenUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ import javax.servlet.http.HttpServletRequest;
  * @version: 0.0.0.1
  */
 @RestController
+@CrossOrigin("*")
 public class UserController {
 
   @Autowired private UserService userService;
